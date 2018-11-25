ce390514445f79f22fa809a3c277995173225274
danutaj23 <danoota@o2.pl>
2018-11-25 23:55:10 +0100

    scenario log in - wip

--- bdd/steps/steps.py
+++ bdd/steps/steps.py
@@ -1,26 +1,42 @@
 from behave import *
+import time
 
 
 @given('the page is loaded')
 def step_impl(context):
-    pass
+    context.browser.get('http://salon.rpgit.pl/')
+    time.sleep(3)
 
 
 @when('I fill username')
 def step_impl(context):
-    pass
+    username_input = context.browser.find_element_by_xpath(
+        '//input[@id="id_username"]'
+    )
+    username_input.send_keys('danuta.klos')
+    time.sleep(3)
 
 
 @when('I fill password')
 def step_impl(context):
-    pass
+    password_input = context.browser.find_element_by_xpath(
+        '//input[@id="id_password"]'
+    )
+    password_input.send_keys('Welcome1')
+    time.sleep(3)
 
 
 @when('I click Zaloguj')
 def step_impl(context):
-    pass
+    login_btn = context.browser.find_element_by_xpath(
+        '//button[@id="id_login_btn"]'
+    )
+    login_btn.click()
+    time.sleep(1)
 
 
 @then('the home page is displayed')
 def step_impl(context):
-    pass
+    context.browser.find_element_by_xpath(
+        '//table[@id="customers"]'
+    )

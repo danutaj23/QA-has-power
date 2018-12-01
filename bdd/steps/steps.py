a75a2dcefed366e73662fdff6e73d83b906cd821
danutaj23 <danoota@o2.pl>
2018-12-01 21:00:02 +0100

    scenario add new car

--- bdd/steps/steps.py
+++ bdd/steps/steps.py
@@ -23,11 +23,50 @@ def step_impl(context, button_id):
         '//button[@id="{button_id}"]'.format(button_id=button_id)
     )
     login_btn.click()
-    time.sleep(3)
+    time.sleep(1)
 
 
 @then('the home page is displayed')
 def step_impl(context):
     context.browser.find_element_by_xpath(
         '//table[@id="customers"]'
+    )
+
+
+@when('I click {"add-new-car"} button')
+def step_impl(context, button_id):
+    login_btn = context.browser.find_element_by_xpath(
+        '//button[@id="{add-new-car}"]'.format(button_id=button_id)
+    )
+    login_btn.click()
+    time.sleep(1)
+
+
+@when('I select "{value}" from "{select_id}" select')
+def step_impl(context, value, select_id):
+    sel = context.browser.find_element_by_xpath(
+        '//select[@id="{select_id}"]/option[text()="{value}"]'
+        .format(select_id=select_id, value=value)
+    )
+    sel.click()
+    time.sleep(1)
+
+
+@when('I click Zapisz')
+def step_impl(context):
+    zapisz_button = context.browser.find_element_by_xpath(
+        '//button[text()="Zapisz"]'
+    )
+    zapisz_button.click()
+    time.sleep(1)
+
+
+@then('there is a car: brand "{brand}", model "{model}", year "{year}"')
+def step_impl(context, brand, model, year):
+    context.browser.find_element_by_xpath(
+        '//table[@id="customers"]'
+        '//tr[td[@class="brand"][text()="{brand}"]]'
+        '[td[@class="model"][text()="{model}"]]'
+        '[td[@class="year"][text()="{year}"]]'
+        .format(brand=brand, model=model, year=year)
     )
--- bdd/zero.feature
+++ bdd/zero.feature
@@ -6,3 +6,11 @@ Feature: Test of car dealer site
     When I type "Welcome1" in "id_password" input
     When I click "id_login_btn" button
     Then the home page is displayed
+
+  Scenario: add a new car
+    When I click "add-new-car" button
+    And I select "Audi" from "id_marka" select
+    And I type "R8" in "id_model" input
+    And I type "2016" in "id_rocznik" input
+    And I click Zapisz
+    Then there is a car: brand "Audi", model "R8", year "2016"
